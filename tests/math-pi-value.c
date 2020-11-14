b612bbe364c3c58f9882a972f79a4d93d0b0ccec
Loredana Sandu <sanduloredanaa@gmail.com>
2020-11-14 18:59:17 +0100

    Created math-pi-value.c to check value of pi in library math.h

---
+++ tests/math-pi-value.c
@@ -0,0 +1,13 @@
+/*
+ * math-pi-value.c
+ * Author: Loredana Sandu
+ * Description: Checks the value of the constant M_PI from library math.h
+ */
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	printf("%.50f", M_PI);
+}
+
